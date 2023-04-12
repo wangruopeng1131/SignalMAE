c24d4da71ded5c4b8d51f436989c047061efeac2
wangruopeng1131 <wangruopeng1131@gmail.com>
2023-04-12 19:50:25 +0800

    10 workers

--- config.py
+++ config.py
@@ -61,4 +61,4 @@ NORM_PIX_LOSS = False
 # resume from checkpoint
 RESUME = True
 if RESUME:
-    RESUME_DIR = Path(__file__).parent.parent / "output_mae/checkpoint-3pth"
+    RESUME_DIR = Path(__file__).parent.parent / "output_mae/checkpoint-3.pth"
