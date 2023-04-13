5541d570ea775e5850b883a2398941da94ddced8
wangruopeng1131 <wangruopeng1131@gmail.com>
2023-04-13 09:07:03 +0800

    o epoch 断点

--- config.py
+++ config.py
@@ -59,6 +59,6 @@ WEIGHT_DECAY = 0.5
 NORM_PIX_LOSS = False
 # torch_ddp = dict(find_unused_parameters=True)  # morlet and resample need this
 # resume from checkpoint
-RESUME = False
+RESUME = True
 if RESUME:
-    RESUME_DIR = Path(__file__).parent.parent / "output_mae/checkpoint-3.pth"
+    RESUME_DIR = Path(__file__).parent.parent / "output_mae/checkpoint-0.pth"
