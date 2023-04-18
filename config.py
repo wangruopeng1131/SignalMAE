424587743479f7da71fddf2ed88dde60f59aeacb
wangruopeng1131 <wangruopeng1131@gmail.com>
2023-04-18 20:02:57 +0800

    resume

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from colossalai.amp import AMP_TYPE
 # ==== Colossal-AI Configuration ====
 
 clip_grad_norm = 2.0
-gradient_accumulation = 5
+gradient_accumulation = 10
 fp16 = dict(mode=AMP_TYPE.TORCH)
 
 DATAPATH = Path(__file__).parent.parent / "icentia11k-single-lead-continuous-raw-electrocardiogram-dataset-1.0"
@@ -37,7 +37,7 @@ WARMUP_EPOCHS = 20 if NUM_EPOCHS > 1 else 0
 CHECKPOINT_INTERVAL = 1
 
 # Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus
-BATCH_SIZE = 24
+BATCH_SIZE = 16
 
 # Place to save pretrained model
 OUTPUT_DIR = Path(__file__).parent.parent / "output_mae"
@@ -49,7 +49,7 @@ MASK_RATIO = 0.75
 # learning rate (absolute lr)
 LEARNING_RATE = 0.002
 # lower lr bound for cyclic schedulers that hit 0
-MINIMUM_LEARNING_RATE = 0.00001
+MINIMUM_LEARNING_RATE = 0.0001
 # base learning rate: absolute_lr = base_lr * total_batch_size / 256
 _BASE_LEARNING_RATE = 0.002
 
